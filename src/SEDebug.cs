50c53f3f197a64a04122dc39d87cc28d16b34b17
wickend <80926418+wick3nd@users.noreply.github.com>
2025-08-16 18:40:55 +0200

    Add files via upload

---
+++ src/SEDebug.cs
@@ -0,0 +1,37 @@
+﻿namespace SEPpackager
+{
+    public enum SEDebugState
+    {
+        Log,
+        Info,
+        Warning,
+        Error
+    };
+
+    class SEDebug
+    {
+        public static void Log<T>(SEDebugState state, T text)
+        {
+            var previousColor = Console.ForegroundColor;
+
+            switch (state)
+            {
+                case SEDebugState.Log: Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case SEDebugState.Info: Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+
+                case SEDebugState.Warning: Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+
+                case SEDebugState.Error: Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+            }
+
+            Console.Write($"  > [{state}]    {text}\n");
+
+            Console.ForegroundColor = previousColor;
+        }
+    }
+}
