eb37b5c1380762537bc86639b826327a2e80e91a
wickend <80926418+wick3nd@users.noreply.github.com>
2025-11-25 23:23:56 +0100

    more compression types + backup

--- src/SEPackager/Compression.cs
+++ src/SEPackager/Compression.cs
@@ -1,71 +1,150 @@
-﻿using SEPackager.CRC;
+﻿using FFMpegCore;
+using FFMpegCore.Enums;
+using FFMpegCore.Pipes;
 using K4os.Compression.LZ4.Streams;
+using SEPackager.CRC;
+using System.IO;
 
 namespace SEPackager
 {
     internal class Compression
     {
-        public static void Copy(FileStream inStream, FileStream outStream) => inStream.CopyTo(outStream);
+        public static void Copy(string path, FileStream outStream)
+        {
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.Read, FileShare.Read);
+            inStream.Position = 0;
+
+            inStream.CopyTo(outStream);
+        }
 
        // Fix the compression
-        public static void CompressLZMA(FileStream inStream, FileStream outStream, out uint fileSize)  // Slower, good ratio
+        public static void CompressLZMA(string path, FileStream outStream, out uint fileSize)  // Slower, good ratio
         {
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.Read, FileShare.Read);
+            inStream.Position = 0;
+
             long streamStart = outStream.Position;
 
-            using var tempStream = new MemoryStream();
+            using var crcStream = new MemoryStream();
             var LZMAEncoder = new SevenZip.Compression.LZMA.Encoder();
             
-            LZMAEncoder.WriteCoderProperties(tempStream);
-            LZMAEncoder.Code(inStream, tempStream, inStream.Length, -1, null);
+            LZMAEncoder.WriteCoderProperties(crcStream);
+            LZMAEncoder.Code(inStream, crcStream, inStream.Length, -1, null);
 
-            uint CRC = CRC32.ComputeChecksum(tempStream.ToArray().AsSpan());
+            uint CRC = CRC32.ComputeChecksum(crcStream.ToArray().AsSpan());
 
-            tempStream.Position = 0;
-            tempStream.CopyTo(outStream);
+            crcStream.Position = 0;
+            crcStream.CopyTo(outStream);
 
             outStream.Write(BitConverter.GetBytes(CRC));          
 
             fileSize = (uint)(outStream.Position - streamStart);
         }
 
-        public static void CompressZSTD(FileStream inStream, FileStream outStream, out uint fileSize, int compresLevel = 5)  // faster, worse ratio
+        public static void CompressZSTD(string path, FileStream outStream, out uint fileSize, int compresLevel = 5)  // faster, worse ratio
         {
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.Read, FileShare.Read);
+            inStream.Position = 0;
+
             long streamStart = outStream.Position;
 
-            using var tempStream = new MemoryStream();
-            using (var ZSTDEncoder = new ZstdSharp.CompressionStream(tempStream, compresLevel)) inStream.CopyTo(ZSTDEncoder);
+            using var crcStream = new MemoryStream();
+            using (var ZSTDEncoder = new ZstdSharp.CompressionStream(crcStream, compresLevel)) inStream.CopyTo(ZSTDEncoder);
 
-            uint CRC = CRC32.ComputeChecksum(tempStream.ToArray().AsSpan());
+            uint CRC = CRC32.ComputeChecksum(crcStream.ToArray().AsSpan());
 
-            tempStream.Position = 0;
-            tempStream.CopyTo(outStream);
+            crcStream.Position = 0;
+            crcStream.CopyTo(outStream);
             
             outStream.Write(BitConverter.GetBytes(CRC));
 
             fileSize = (uint)(outStream.Position - streamStart);
         }
 
-        public static void CompressLZ4(FileStream inStream, FileStream outStream, out uint fileSize, int compresLevel = 5)
+        public static void CompressLZ4(string path, FileStream outStream, out uint fileSize, int compresLevel = 5)
         {
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.Read, FileShare.Read);
+            inStream.Position = 0;
+
             long streamStart = outStream.Position;
 
-            using var tempStream = new MemoryStream();
+            using var crcStream = new MemoryStream();
             var settings = new LZ4EncoderSettings() { CompressionLevel = (K4os.Compression.LZ4.LZ4Level)compresLevel };
 
-            using var LZ4Encoder = LZ4Stream.Encode(tempStream, settings);
+            using var LZ4Encoder = LZ4Stream.Encode(crcStream, settings);
             inStream.CopyTo(LZ4Encoder);
             LZ4Encoder.Flush();
 
-            uint CRC = CRC32.ComputeChecksum(tempStream.ToArray().AsSpan());
+            uint CRC = CRC32.ComputeChecksum(crcStream.ToArray().AsSpan());
 
-            tempStream.Position = 0;
-            tempStream.CopyTo(outStream);
+            crcStream.Position = 0;
+            crcStream.CopyTo(outStream);
 
             outStream.Write(BitConverter.GetBytes(CRC));
 
             fileSize = (uint)(outStream.Position - streamStart);
         }
 
-       // Add other compressions
+        public static void SoundToOGG(string path, FileStream outStream, out uint fileSize)  // Add CRC for fucks sake
+        {
+           // Creates a temporary ogg buffer
+           // string tempFilePath = $"{Program.soundBufferPath}";
+           // using var tempStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+           // File.SetAttributes(tempFilePath, FileAttributes.Hidden);
+
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+            long streamStart = outStream.Position;
+
+                FFMpegArguments.FromPipeInput(new StreamPipeSource(inStream))
+                    .OutputToPipe(new StreamPipeSink(outStream), options => options
+                        .WithAudioCodec("libopus")
+                       // .WithAudioBitrate(AudioQuality.Good)
+
+                        .ForceFormat("ogg"))
+                    .ProcessSynchronously();
+
+            fileSize = (uint)(outStream.Position - streamStart);
+        }
+
+        public static void VideoToVG9(string path, FileStream outStream, out uint fileSize)
+        {
+            using var inStream = new FileStream($"{path}", FileMode.Open, FileAccess.Read, FileShare.Read);
+            inStream.Position = 0;
+
+            long streamStart = outStream.Position;
+            using var crcStream = new MemoryStream();
+
+            var videoInfo = FFProbe.Analyse(path);
+            var videoBitrate = videoInfo.PrimaryVideoStream!.BitRate;
+            var videoFremarate = videoInfo.PrimaryVideoStream!.FrameRate;
+
+            FFMpegArguments.FromPipeInput(new StreamPipeSource(inStream))
+                .OutputToPipe(new StreamPipeSink(crcStream), options => options
+                    .WithAudioCodec("libopus")
+
+                    .WithVideoCodec("libvpx-vp9")
+                    .WithFramerate(videoFremarate)
+                    .WithVideoBitrate((int)videoBitrate)
+
+                    .ForceFormat("webm"))
+                .ProcessSynchronously();
+
+            uint CRC = CRC32.ComputeChecksum(crcStream.ToArray().AsSpan());
+
+            crcStream.Position = 0;
+            crcStream.CopyTo(outStream);
+
+            outStream.Write(BitConverter.GetBytes(CRC));
+
+            fileSize = (uint)(outStream.Position - streamStart);
+        }
+
+        public static void ImageToBC()
+        {
+
+        }
+
+       // Add other compressions  - maybe
     }
 }
